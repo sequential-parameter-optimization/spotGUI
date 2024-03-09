0ed21e4f51c2512968cac6fc54607bca6b67d9c0
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2024-03-09 18:45:53 +0100

    0.0.31
    test fixed

--- test/test_spotRun.py
+++ test/test_spotRun.py
@@ -8,7 +8,7 @@ from spotPython.spot import spot
 
 def test_run_spot_python_experiment_save_only():
     # Define the input parameters for the function
-    save_only = True 
+    save_only = True
     fun_control = fun_control_init(PREFIX="test")
     design_control = design_control_init()
     surrogate_control = surrogate_control_init()
@@ -27,6 +27,6 @@ def test_run_spot_python_experiment_save_only():
         optimizer_control,
     )
     # Assert the expected output
-    assert len(result) == 6
+    assert len(result) == 7
     assert result[0] is not None 
     assert isinstance(result[1], spot.Spot)  # spot_tuner should be an instance of spot.Spot
